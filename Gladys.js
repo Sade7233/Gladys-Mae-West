880cf5caf6cd786548919759b5dc3d129c3c0928
Sade7233 <73080623+Sade7233@users.noreply.github.com>
2020-12-17 13:00:47 -0500

    Added functionality to Fun Fact Buttons #2 and #3

--- Gladys.js
+++ Gladys.js
@@ -11,6 +11,20 @@ FactParagraph.innerHTML="Gladys is a member of the Alpha Kappa Alpha (AKA) soror
 
 Fact1.addEventListener("click", funFact1);
 
+function funFact2 () {
+  //when Fact 2 button clicked, paragraph changes to Fun Fact
+FactParagraph.innerHTML="Gladys actually favors paper maps over the electronic GPS system because she would rather rely on her own calculations than risk that the system's data points are wrong or outdated."
+}
+
+Fact2.addEventListener("click", funFact2);
+
+function funFact3 () {
+  //when Fact 3 button clicked, paragraph changes to Fun Fact
+FactParagraph.innerHTML="Gladys met her husband, a fellow mathematician, at work. He was one of ther other 3 black folks who worked at the naval base!"
+}
+
+Fact3.addEventListener("click", funFact3);
+
 /*she is a member of the Alpha Kappa Alpha (AKA) sorority
 2) she actually favors paper maps over the electronic GPS system because she'd rather rely on her own calculations than risk that the system's data points are wrong or outdated
 3) She met her husband, a fellow mathematician (how cuteee!) at work (surprise! he was one of*/
